284bdd03d95ff34f20e53da46c26cc517620bdc1
124476 <124476@bk.ru>
2024-07-25 17:46:49 +0300

    Uplode program

---
+++ Nakrytka/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Nakrytka.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Nakrytka
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                int kol = Int32.Parse(KolText.Text);
+                for (int i = 0; i < kol; i++)
+                {
+                    var dialog = new OknoNack(SrlText.Text, Int32.Parse(TimeText.Text));
+                    dialog.Show();
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Error");
+            }
+        }
+    }
+}
---
+++ Nakrytka/Windows/OknoNack.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace Nakrytka.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для OknoNack.xaml
+    /// </summary>
+    public partial class OknoNack : Window
+    {
+        DispatcherTimer dispatcherTimer;
+        public OknoNack(string srl, int time)
+        {
+            InitializeComponent();
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(time);
+            dispatcherTimer.Tick += SetTime;
+            WebW.Source = new Uri(srl);
+        }
+
+        private void SetTime(object sender, EventArgs e)
+        {
+            dispatcherTimer.Stop();
+            WebW.Source = new Uri("https://google.com");
+        }
+    }
+}
